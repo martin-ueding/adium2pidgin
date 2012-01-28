e39075dc18b449fa624f2e2fee223172942f1ca3
Martin Ueding <dev@martin-ueding.de>
2012-01-28 23:56:05 +0100

    Document

--- writer.py
+++ writer.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 # Copyright (c) 2012 Martin Ueding <dev@martin-ueding.de>
 
-# Me
-# Other
-
 import dateutil.parser
 
 def write_pidgin(chat, outfile):
+    """
+    Writes a chat dict as pidgin HTML file.
+
+    @param chat: Dict with chat.
+    @type chat: dict
+    @param outfile: File to write to.
+    @type outfile: file
+    """
     outfile.write('<html><head><meta http-equiv="content-type" content="text/html; charset=UTF-8"><title>Conversation with 307611255 at Mo 01 Aug 2011 10:45:58 CEST on 307259554 (icq)</title></head><body><h3>Conversation with 307611255 at Mo 01 Aug 2011 10:45:58 CEST on 307259554 (icq)</h3>\n')
     for message in chat["messages"]:
         if message["sender"] == chat["account"]:
