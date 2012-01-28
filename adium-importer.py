92bfb606cec28872a15c54fc26b56c232cebd8d8
Martin Ueding <dev@martin-ueding.de>
2012-01-28 22:47:34 +0100

    Init

---
+++ adium-importer.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2012 Martin Ueding <dev@martin-ueding.de>
+
+from xml.dom.minidom import parse
+import xml
+import optparse
+
+def main():
+    options, args = _parse_args()
+
+    filename = args[0]
+
+    tree = parse(filename)
+    
+    chatobject = tree.childNodes[0]
+    messages = chatobject.childNodes
+
+    for message in messages:
+        if not message.nodeName == "message":
+            continue
+
+
+        sender = message.getAttribute("sender")
+
+        child = message
+        while child.nodeType != xml.dom.Node.TEXT_NODE:
+            child = child.childNodes[0]
+
+        text = child.nodeValue
+
+        print sender.rjust(20), ":", text
+
+
+
+def _parse_args():
+    """
+    Parses the command line arguments.
+
+    @return: Tuple with options and (positional) arguments.
+    @rtype: tuple
+    """
+    parser = optparse.OptionParser(usage="", description="")
+    #parser.add_option("", dest="", type="", default=, help=)
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+	main()
