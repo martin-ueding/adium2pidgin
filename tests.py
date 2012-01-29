ce2cc57d6dcf3eda977a908c886a7a58a79a7c90
Martin Ueding <dev@martin-ueding.de>
2012-01-29 13:51:01 +0100

    Add simple unittest class

---
+++ tests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2012 Martin Ueding <dev@martin-ueding.de>
+
+import unittest
+
+import chatlogparser
+
+class Tests(unittest.TestCase):
+    def testAdiumParser(self):
+        assert True
+
+if __name__ == "__main__":
+    unittest.main()
